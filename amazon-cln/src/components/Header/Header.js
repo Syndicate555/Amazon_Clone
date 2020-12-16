7033762af5a5cae6a799e592f6bb19c0b98f8543
Syndicate555 <saziz028@uottawa.ca>
2020-12-16 16:34:36 -0500

    Update Header.js

--- amazon-cln/src/components/Header/Header.js
+++ amazon-cln/src/components/Header/Header.js
@@ -4,6 +4,7 @@ function Header() {
  return (
   <div className= 'header'>
    <img className = "header_logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""/>
+   <div className ='header_search'></div>
   </div>
  )
 }
