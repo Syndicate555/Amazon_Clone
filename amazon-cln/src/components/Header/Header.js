a67e3e055e1c397f16c52e5d256e77fcca87df23
Syndicate555 <saziz028@uottawa.ca>
2020-12-16 16:27:25 -0500

    a

--- amazon-cln/src/components/Header/Header.js
+++ amazon-cln/src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
  return (
   <div className= 'header'>
-   
+   <img src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""/>
   </div>
  )
 }
