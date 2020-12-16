fc0b61a65eaa634e866a36448c05f9c66cfd5336
Syndicate555 <saziz028@uottawa.ca>
2020-12-16 16:30:16 -0500

    s

--- amazon-cln/src/components/Header/Header.js
+++ amazon-cln/src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
  return (
   <div className= 'header'>
-   <img src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""/>
+   <img src="https://www.clipartmax.com/png/middle/462-4625003_where-we-shop-transparent-background-amazon-in-logo-png.png" alt=""/>
   </div>
  )
 }
