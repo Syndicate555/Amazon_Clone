9e7cc260d4cd3df9f520fe39a8869e99e2b13733
Syndicate555 <saziz028@uottawa.ca>
2020-12-16 16:17:08 -0500

    Create firebase.js

---
+++ amazon-cln/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCQb3vT5TWFerzKXvbHSCL-NLvHxJ4yqZ8",
+  authDomain: "cln-53f2e.firebaseapp.com",
+  projectId: "cln-53f2e",
+  storageBucket: "cln-53f2e.appspot.com",
+  messagingSenderId: "589789735242",
+  appId: "1:589789735242:web:d010e90f02cc5baec5f9b3",
+  measurementId: "G-QJ2DHGHMG9"
+};
