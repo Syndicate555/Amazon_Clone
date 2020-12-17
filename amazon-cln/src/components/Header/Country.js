42daa7b31b9286d11a230432e1085897e6e116f4
Syndicate555 <saziz028@uottawa.ca>
2020-12-16 19:35:53 -0500

    Create Country.js

---
+++ amazon-cln/src/components/Header/Country.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Country extends Component {
+ constructor() {
+  super();
+  this.state = {
+   showMenu: false
+  };
+ }
+ render() {
+  return (
+   <div>
+    
+   </div>
+  )
+ }
+}
