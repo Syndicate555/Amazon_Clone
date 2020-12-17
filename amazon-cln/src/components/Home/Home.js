3b7c67953c1c01081f42297578c022b84c2021f5
Syndicate555 <saziz028@uottawa.ca>
2020-12-16 20:50:31 -0500

    r

--- amazon-cln/src/components/Home/Home.js
+++ amazon-cln/src/components/Home/Home.js
@@ -4,7 +4,8 @@ function Home() {
  return (
   <div className = "home">
    <div className="home__container">
-    
+    <img className= "home__image" src="https://images-na.ssl-images-amazon.com/images/G/15/digital/video/merch/2020/Other/BRND_MTH20_00000_GWBleedingHero_1500x600_Final_en-CA_ENG_PVD6150._CB416179696_.jpg" alt=""/>
+
    </div>
   </div>
  )
