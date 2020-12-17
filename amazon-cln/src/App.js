d2ad3f60a5bfdd3b47cbde08b2af6e5d165560a5
Syndicate555 <saziz028@uottawa.ca>
2020-12-16 20:47:48 -0500

    w

--- amazon-cln/src/App.js
+++ amazon-cln/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div >
       <Header/>
-     
+     <Home/>
      {/* Header*/}
      {/* Home*/}
     </div>
--- amazon-cln/src/components/Home/Home.js
+++ amazon-cln/src/components/Home/Home.js
@@ -3,7 +3,9 @@ import './Home.css'
 function Home() {
  return (
   <div className = "home">
-   
+   <div className="home__container">
+    
+   </div>
   </div>
  )
 }
