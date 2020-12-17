6aeeb30650cb9c841dc85b2673061217927847d9
Syndicate555 <saziz028@uottawa.ca>
2020-12-16 20:16:12 -0500

    e

--- amazon-cln/src/components/Header/Header.js
+++ amazon-cln/src/components/Header/Header.js
@@ -57,7 +57,7 @@ function Header() {
     </div>
     <div className= 'header_optionBasket'>
      <ShoppingBasketIcon />
-     <span className = "header__optionLineTwo header__basketCount">0</span>
+     <span className = "header__optionLineTwo header_basketCount">0</span>
     </div>
    </div>
   </div>
