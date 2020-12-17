a1b7ffae46b07e46da7711a3209418b65715a19f
Syndicate555 <saziz028@uottawa.ca>
2020-12-16 19:52:32 -0500

    e

--- amazon-cln/src/components/Header/Header.js
+++ amazon-cln/src/components/Header/Header.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from "@material-ui/icons/Search"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
+import Location from './assets/location.jpg'
+
+
 function Header() {
  return (
   <div className= 'header'>
-   <img className = "header__logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""/>
+   <img className="header__logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+   <img className="header__location" src={Location} alt="" />
+   
    <div className='header__search'>
     <input type="text" className="header__searchInput" />
     <SearchIcon className = "header__searchIcon"/>
    </div>
    <div className='header_nav'>
     <div className = "header_country">
-     
+     <DropdownButton
+      alignRight
+      title="Canada"
+      id="dropdown-menu-align-right"
+    
+        >
+              <Dropdown.Item eventKey="option-1">Canada</Dropdown.Item>
+              <Dropdown.Item eventKey="option-2">US</Dropdown.Item>
+              <Dropdown.Item eventKey="option-3">UK</Dropdown.Item>
+              
+      </DropdownButton>
     </div>
     <div className="header_option">
      <span className = "header__optionLineOne">
